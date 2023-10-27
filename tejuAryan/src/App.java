1126a9fca705255e156c5fa636c6f367fc906d19
AKherb <a.kashyapgah@gmail.com>
2023-10-27 11:35:13 -0400

    Aryan Kashyap Commit

---
+++ tejuAryan/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        //Aryan Has Edited This Document 
+        System.out.println("Hello, World!");
+    }
+}
