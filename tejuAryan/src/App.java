3219d6a8f0849b0921fb866314a6276bfc07946e
TejasRaghuram <62579368+TejasRaghuram@users.noreply.github.com>
2023-10-27 11:42:27 -0400

    Tejas's Commit
    This commit is the single best commit on this repository.

--- tejuAryan/src/App.java
+++ tejuAryan/src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         //Aryan Has Edited This Document 
+        //Tejas has edited this document as well, but better.
         System.out.println("Hello, World!");
     }
 }
